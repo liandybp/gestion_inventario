15585976eb79cbcfbc98e33095953ff0ac32da16
jecortes <jecortes2304@gmail.com>
2026-01-01 19:34:42 +0100

    Changed docekrfile

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from __future__ import annotations
 
+import os
+
 import uvicorn
 
 
 def main() -> None:
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
+    app_port = os.getenv("APP_PORT", "8000")
+    uvicorn.run("app.main:app", host="127.0.0.1", port=int(app_port), reload=True)
 
 
 if __name__ == "__main__":
